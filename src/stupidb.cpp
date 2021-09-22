aa5a90829d769745656e6240796ccee254300141
codea-ppp <yangshuxuanplus@gmail.com>
2021-09-22 23:04:40 +0800

    add another constructor of stupidb

--- src/stupidb.cpp
+++ src/stupidb.cpp
@@ -33,6 +33,32 @@ std::shared_ptr<stupidb> stupidb::get_instance(const char* config_path)
 	return registe[key];
 }
 
+std::shared_ptr<stupidb> stupidb::get_instance(const char* host, 
+											   const char* user,
+											   const char* password,
+											   const char* db,
+											   unsigned int port,
+											   const char* sock, 
+											   const unsigned int max_connection)
+{
+	static std::mutex lk;
+	std::scoped_lock<std::mutex> guard(lk);
+
+	dbargs args(host, user, password, db, port, sock, max_connection);
+	if (!args._is_right)
+		throw std::runtime_error("bad config file");
+
+	std::string key(args._user);
+	key.append(args._password);
+	key.append(args._db);
+
+	if (registe.count(key))
+		return registe[key];
+
+	registe[key] = std::shared_ptr<stupidb>(new stupidb(args));
+	return registe[key];
+}
+
 int stupidb::query(const std::string& statment, column_ret_pt accu) const
 {
 	return impls[++impl_index % impl_length].query(statment, accu);
--- src/stupidb.h
+++ src/stupidb.h
@@ -18,6 +18,13 @@ class stupidb
 {
 public:
 	static std::shared_ptr<stupidb> get_instance(const char* config_key);
+	static std::shared_ptr<stupidb> get_instance(const char* host, 
+												 const char* user,
+												 const char* password,
+												 const char* db,
+												 unsigned int port,
+												 const char* sock, 
+												 const unsigned int max_connection);
 
 	int query(const std::string& statment, column_ret_pt accu) const;
 	int query(const std::string& statment, row_ret_pt accu) const;
